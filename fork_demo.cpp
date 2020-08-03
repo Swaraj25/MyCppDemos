462feee0195f054f931a3cf56a7053a6e3aa83a0
Swaraj Gore <swarajgore99@gmail.com>
2020-08-03 12:07:19 +0530

    Added new variable b

--- fork_demo.cpp
+++ fork_demo.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+#include<zconf.h>
+using namespace std;
+int main(){
+    int p = 10;
+    int *a = &p;
+    printf("%d\n",*a);
+    fork();
+    p++;
+    printf("%d\n",*a);
+    return 0;
+}
